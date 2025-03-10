ddfb5690b1a2807e7df677945953b82e77fee44b
alfah23 <alfonso.arzave23@hotmail.com>
2025-03-09 22:34:06 -0600

    La verdad solo le avance poquito a la pantalla NuevaVisita

--- lib/screens/residentes/nueva_visita_screen.dart
+++ lib/screens/residentes/nueva_visita_screen.dart
@@ -7,6 +7,48 @@ class NuevaVisitaScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink.shade400,
+      body: ContenidoNuevaVisita()
     );
+  }
+}
+
+class ContenidoNuevaVisita extends StatefulWidget {
+  const ContenidoNuevaVisita({super.key});
+
+  @override
+  State<ContenidoNuevaVisita> createState() => _ContenidoNuevaVisitaState();
+}
+
+class _ContenidoNuevaVisitaState extends State<ContenidoNuevaVisita> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Stack(children: [
+          FondoNuevaVisita(),
+          FormularioNuevaVisita()
+        ],)
+      ),
+    );
+  }
+}
+
+class FondoNuevaVisita extends StatelessWidget {
+  const FondoNuevaVisita({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade400,
+    );
+  }
+}
+
+class FormularioNuevaVisita extends StatelessWidget {
+  const FormularioNuevaVisita({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
